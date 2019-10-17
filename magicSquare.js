49fb7cf2c9b02a13985576f968a275139338ec9a
pixelynx <tjstevens1992@gmail.com>
2019-10-16 20:31:06 -0400

    magic square

--- magicSquare.js
+++ magicSquare.js
@@ -1,5 +1,5 @@
 // accepts a square matrix, will return boolean on whether or not it is magicSquare
-function isMagicSquare(mat) {
+const isMagicSquare = (mat) => {
     const n = mat.length;
     let diagonalSum = 0;
     let diagonalSum2 = 0;
@@ -34,8 +34,42 @@ function isMagicSquare(mat) {
     return true;
 }
 
+const mtx = [[2, 7, 6], [9, 5, 1], [4, 3, 8]]
+
+console.log(isMagicSquare(mtx))
+
+// create all combinations for different row / col ranges
+const generateAllCombinations = (val) => {
+    const combinations = [];
+    for (let startIndex = 0; startIndex < val; startIndex++) {
+        for (let endIndex = startIndex + 1; endIndex < val + 1; endIndex++) {
+            combinations.push([startIndex, endIndex]);
+        }
+    }
+    return combinations;
+}
+
+// create sub-matrix with row/col range
+const generateSubMatrix = (
+    mat,
+    startingRow,
+    endingRow,
+    startingCol,
+    endingCol
+) => {
+    const subMat = [];
+    for (let i = startingRow; i < endingRow; i++) {
+        const row = [];
+        for (let j = startingCol; j < endingCol; j++) {
+            row.push(mat[i][j]);
+        }
+        subMat.push(row);
+    }
+    return subMat;
+}
+
 // generate all sub-matrices:
-function generateSubMatrices(mat) {
+const generateSubMatrices = (mat) => {
     // get max val of possible rows / cols sub-matrices can have
     const rows = mat.length;
     const cols = mat[0].length;
@@ -61,36 +95,6 @@ function generateSubMatrices(mat) {
     return subMatrices;
 }
 
-// create all combinations for different row / col ranges
-function generateAllCombinations(val) {
-    const combinations = [];
-    for (let startIndex = 0; startIndex < val; startIndex++) {
-        for (let endIndex = startIndex + 1; endIndex < val + 1; endIndex++) {
-            combinations.push([startIndex, endIndex]);
-        }
-    }
-    return combinations;
-}
-
-// create sub-matrix with row/col range
-function generateSubMatrix(
-    mat,
-    startingRow,
-    endingRow,
-    startingCol,
-    endingCol
-) {
-    const subMat = [];
-    for (let i = startingRow; i < endingRow; i++) {
-        const row = [];
-        for (let j = startingCol; j < endingCol; j++) {
-            row.push(mat[i][j]);
-        }
-        subMat.push(row);
-    }
-    return subMat;
-}
-
 const mat = [[2, 7, 6], [9, 5, 1], [4, 3, 8]];
 
 // get sub-matrices minus the single val and non square ones
@@ -98,6 +102,6 @@ const subMats = generateSubMatrices(mat).filter(
     subM => subM.length === subM[0].length && subM.length > 1
 );
 
-for (let mat of subMats) {
-    console.log(isMagicSquare(mat), mat, mat.length);
-}
+// for (let mat of subMats) {
+//     console.log(isMagicSquare(mat), mat, mat.length);
+// }
