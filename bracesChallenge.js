09930f4fd4c5c279a4ac89dbd32edfcaf5695f3c
pixelynx <tjstevens1992@gmail.com>
2019-10-28 18:18:49 -0400

    i cannot

--- bracesChallenge.js
+++ bracesChallenge.js
@@ -1,23 +1,49 @@
 // Basic matching braces logic
 
-const isMatchingBrace = (str) => {
-    let basicBxtchStack = [];
+// const isMatchingBrace = (str) => {
+//     let basicBxtchStack = [];
+//     let braces = {
+//         '(': ')',
+//         '[': ']',
+//         '{': '}'
+//     }
+
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === '(' || str[i] === '[' || str[i] === '{') basicBxtchStack.push(str[i])
+//         else {
+//             let last = basicBxtchStack.pop();
+//             if (str[i] !== braces[last]) return false;
+//         }
+//     }
+//     if (basicBxtchStack.length !== 0) return false;
+
+//     return true;
+// }
+
+// console.log(isMatchingBrace(['()()']))
+
+const isMatchingBrace = (arr) => {
+    let result = [];
     let braces = {
         '(': ')',
         '[': ']',
         '{': '}'
     }
 
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === '(' || str[i] === '[' || str[i] === '{') basicBxtchStack.push(str[i])
-        else {
-            let last = basicBxtchStack.pop();
-            if (str[i] !== braces[last]) return false;
+    let basicBxtchStack = [];
+    for (let i = 0; i < arr.length; i++) {
+        let str = arr[i];
+        for (let j = 0; j < str.length; j++) {
+            if (str[j] === '(' || str[j] === '[' || str[j] === '{') basicBxtchStack.push(str[j]);
+            else {
+                let last = basicBxtchStack.pop();
+                result.push(braces[j] !== braces[last] ? 'NO' : 'YES')
+            }
         }
     }
-    if (basicBxtchStack.length !== 0) return false;
-
-    return true;
+    return result;
 }
 
-console.log(isMatchingBrace(['()()']))
+let arr1 = ['((})', '()[]{}', '((})', '({[[]]})'];
+
+console.log(isMatchingBrace(arr1))
