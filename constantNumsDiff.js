c98f0f26a12ed104499d1f2c766eb2fc6b6b1f94
pixelynx <tjstevens1992@gmail.com>
2019-10-29 18:58:21 -0400

    working on solution to constant numbers

---
+++ constantNumsDiff.js
@@ -0,0 +1,9 @@
+let pairs = [1 + ' ' + 2, 3 + ' ' + 4];
+const returnPairs = (arr) => {
+    for (let i = 0; i < pairs.length; i++) {
+        console.log(pairs[i])
+    }
+}
+
+returnPairs(pairs)
+// console.log(returnPairs(pairs))
