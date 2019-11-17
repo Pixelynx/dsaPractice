a016cdbfdaccaf01603ee5e79ff130971ccfd74e
pixelynx <tjstevens1992@gmail.com>
2019-11-16 19:13:16 -0500

    optimized valid braces

---
+++ constantIsValidBraces.js
@@ -0,0 +1,23 @@
+var isValid = function (s) {
+    let basicStack = [];
+    let braces = {
+        '{': '}',
+        '[': ']',
+        '(': ')'
+    }
+
+    for (let el of s) {
+
+        let top = basicStack[basicStack.length - 1]
+        if (el === '(' || el === '[' || el === '{') {
+            basicStack.push(el)
+        } else {
+            if (el === braces[top]) {
+                basicStack.pop()
+            } else {
+                return false
+            }
+        }
+    }
+    return basicStack.length < 1
+};
