2f2c7f0e8249c512ec80c1d904f462fb91b37f4b
pixelynx <tjstevens1992@gmail.com>
2019-11-21 03:04:41 -0500

    stuck on reverse list

--- linkedListPractice.js
+++ linkedListPractice.js
@@ -25,23 +25,46 @@ class LinkedList {
     }
 
     removeNode() {
-        if(this.head === null) return null;
-        else {
-            let current = this.head;
-            while(current) {
-                current = current.next;
-            }
-            // current.next 
+        if (this.head === null) return null;
+        if (this.head.next === null) {
+            this.head = null;
+            return;
         }
+        else {
+            let prev = this.head;
+            let tail = this.head.next;
+            while (tail.next !== null) {
+                prev = tail;
+                tail = tail.next;
+            }
+            prev.next = null;
+            return;
+        }
+    }
+
+    reverseNode(node, prev) {
+        let newNode = new Node(node.value, prev);
+        if (node.next) {
+            return this.reverseNode(node.next, newNode)
+        } else return newNode;
+    }
+
+    reverseList() {
+        // keep track of the current node
+        // keep track of when end of list is reached
+        // assign this.head to last node
+
+        // if(this.head === null) return null;
+        // if(this.head.next === null) return;
+        return this.reverseNode(this.head, null);
+
     }
 
     printList() {
         let current = this.head;
-        console.log(current)
         let str = "";
         while (current) {
-
-            str += current.value + " " + "=>" + " ";
+            str += current.value + " => ";
             current = current.next;
         }
         return str;
@@ -53,4 +76,6 @@ list.addNode(5);
 list.addNode(56);
 list.addNode(23);
 list.addNode(3);
+// list.removeNode();
+list.reverseList()
 list.printList()
