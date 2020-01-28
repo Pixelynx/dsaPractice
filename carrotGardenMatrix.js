229bb551080f320a9d003ec1897383143d228620
pixelynx <tjstevens1992@gmail.com>
2020-01-27 23:29:03 -0500

    working out algorithm logic

--- carrotGardenMatrix.js
+++ carrotGardenMatrix.js
@@ -9,6 +9,15 @@ let mid = Math.floor(mtx.length / 2)
 let count = 0;
 
 for (let i = 0; i <= mtx.length; i++) {
-    
+    // CREATE LOOP
+    // CREATE VARIABLE FOR TOP => [ROW-1][COL]
+    // CREATE VARIABLE FOR LEFT => [ROW][COL-1]
+    // CREATE VARIABLE FOR RIGHT => [ROW][COL+1]
+    // CREATE VARIABLE FOR BOTTOM => [ROW+1][COL]
+    // GRAB INDEX MIDDLE MOST ARRAY(S)
+    // GRAB MIDDLE INDEX OF ARRAY
+    // IF EVEN NUMBER OF ROWS, CHECK IF LEFT, RIGHT, OR BOTTOM ARE GREATER
+        // IF TRUE -- GRAB INDEX OF GREATEST NUMBER
+        // 
 
 }
