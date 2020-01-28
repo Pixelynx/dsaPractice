f98c8ae2fa7e875618960e105514d0503f3752ed
pixelynx <tjstevens1992@gmail.com>
2020-01-27 23:04:55 -0500

    going back to algorithm

--- carrotGardenMatrix.js
+++ carrotGardenMatrix.js
@@ -9,31 +9,6 @@ let mid = Math.floor(mtx.length / 2)
 let count = 0;
 
 for (let i = 0; i <= mtx.length; i++) {
-    let idx;
-    let midIdx;
-    let highestNum = 0;
+    
 
-    if (mtx % 2) {
-        midIdx = mtx[mid]
-    } else {
-        // IF MATRIX HAS ODD NUMBER OF ROWS
-        let currentHigh = 0;
-        midIdx = [mtx[mid - 1], mtx[mid]]
-        // console.log(midIdx)
-        // LOOP TWO MIDDLE ROWS
-        for (let j = 0; j < midIdx.length; j++) {
-            let currentMid = midIdx[j]
-            let currentMidNumIdx;
-            if (currentMid[Math.floor(currentMid.length / 2)] > currentHigh) {
-                currentHigh = currentMid[Math.floor(currentMid.length / 2)]
-                currentMidNumIdx = Math.floor(currentMid.length / 2);
-                if(currentMid > currentMid+1 && currentMid > currentMid-1) {
-                    console.log('current middle is greatest')
-                } else console.log("there's a larger num than current mid")
-                
-            }
-        }
-        console.log(currentHigh)
-    }
-    // console.log(highestNum)
 }
