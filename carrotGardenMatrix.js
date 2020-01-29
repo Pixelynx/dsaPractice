c3bf406539620b73fdb06de7d9892774efde327c
pixelynx <tjstevens1992@gmail.com>
2020-01-28 20:23:17 -0500

    added comments

--- carrotGardenMatrix.js
+++ carrotGardenMatrix.js
@@ -29,14 +29,25 @@ const carrotGarden = (mtx) => {
         let currentMidSquares = [];
         let current = [];
         
+        // IF MATRIX HAS EVEN NUMBER OF ROWS, PUSH TWO MIDDLE ARRAYS
         if(!(mid%2)) {
             currentMidSquares.push(mtx[mid - 1], mtx[mid]) // => currentMidSquares = [[0, 0, 7, 0, 4], [4, 6, 3, 4, 9]]
+            // currrentMidSquares ZEROTH INDEX[0, 0], FIRST INDEX[0, 1] index values => [[1, 2], [2, 2]]
         } 
         
-        current = currentMidSquares[0][mid] > currentMidSquares[[1][mid]] ? [[0], [mid]] : [1, mid]
+        // IF MIDDLE INDEX OF FIRST MIDDLE ARRAY IS GREATER THAN THE MIDDLE INDEX OF SECOND MIDDLE ARRAY, REDEFINE "CURRENT" WITH ZEROTH INDEX VALUE OF THE CURRENT MIDDLE SQUARES' INDEX => 1; 
+            // ELSE IF MIDDLE INDEX OF SECOND MIDDLE ARRAY IS GREATER THAN THE MIDDLE INDEX OF THE FIRST MIDDLE ARRAY, REDEFINE "CURRENT" WITH FIRST INDEX VALUE OF THE CURRENT MIDDLE SQUARES' INDEX => 2
+        current = currentMidSquares[0][mid] > currentMidSquares[[1][mid]] ? [0, [mid]] : [1, mid]
         
+        // DEFINE VARIABLES FOR LEFT AND RIGHT OF CURRENT VALUES; FOR READABILITY
         let left = [current[0], current[1]-1];
         let right = [current[0], current[1]+1];
+
+        // CHECK IF LEFT OR RIGHT ELEMENTS OF CURRENT INDEX HAVE A GREATER ELEMENT VALUE
+        if (mtx[left[0]][left[1]] > mtx[current[0][current[1]]]) current = [left[0]][left[1]];
+        if (mtx[right[0]][right[1]] > mtx[current[0][current[1]]]) current = [right[0]][right[1]];
+
+        console.log(mtx[left[0]][left[1]])
 
         for(col = 0; col < mtx.length; col++) {
             row = current[0], col = current[1]
