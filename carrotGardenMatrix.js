b3ed791dbeb7d46a4f62b77fbc651c2f30d46de7
pixelynx <tjstevens1992@gmail.com>
2020-01-28 20:48:26 -0500

    working on utilizing Math.max

--- carrotGardenMatrix.js
+++ carrotGardenMatrix.js
@@ -24,43 +24,49 @@ const carrotGarden = (mtx) => {
 
     let carrotsEaten = 0;
     let mid = Math.floor(mtx.length / 2)
-
+    
     for(let row = 0; row <= mtx.length; row++) {
         let currentMidSquares = [];
-        let current = [];
+        let currentIdx = [];
         
         // IF MATRIX HAS EVEN NUMBER OF ROWS, PUSH TWO MIDDLE ARRAYS
         if(!(mid%2)) {
             currentMidSquares.push(mtx[mid - 1], mtx[mid]) // => currentMidSquares = [[0, 0, 7, 0, 4], [4, 6, 3, 4, 9]]
             // currrentMidSquares ZEROTH INDEX[0, 0], FIRST INDEX[0, 1] index values => [[1, 2], [2, 2]]
+
+            // IF MIDDLE INDEX OF FIRST MIDDLE ARRAY IS GREATER THAN THE MIDDLE INDEX OF SECOND MIDDLE ARRAY, REDEFINE "CURRENT" WITH ZEROTH INDEX VALUE OF THE CURRENT MIDDLE SQUARES' INDEX => 1; 
+            // ELSE IF MIDDLE INDEX OF SECOND MIDDLE ARRAY IS GREATER THAN THE MIDDLE INDEX OF THE FIRST MIDDLE ARRAY, REDEFINE "CURRENT" WITH FIRST INDEX VALUE OF THE CURRENT MIDDLE SQUARES' INDEX => 2
+            currentIdx = currentMidSquares[0][mid] > currentMidSquares[[1][mid]] ? [0, [mid]] : [1, mid]
+
+            // DEFINE VARIABLES FOR LEFT AND RIGHT OF CURRENT VALUES; FOR READABILITY
+            let left = [currentIdx[0], currentIdx[1] - 1];
+            let right = [currentIdx[0], currentIdx[1] + 1];
+
+            // CHECK IF LEFT OR RIGHT ELEMENTS OF CURRENT INDEX HAVE A GREATER ELEMENT VALUE
+            if (mtx[left[0]][left[1]] > mtx[currentIdx[0][currentIdx[1]]]) currentIdx = [left[0]][left[1]];
+            if (mtx[right[0]][right[1]] > mtx[currentIdx[0][currentIdx[1]]]) currentIdx = [right[0]][right[1]];
+
+            carrotsEaten += mtx[currentIdx[0]][[currentIdx[1]]];
+            
+            // console.log(carrotsEaten, mtx[current[0]][[current[1]]])
+            // console.log(mtx[current[0]][current[1]])
         } 
         
-        // IF MIDDLE INDEX OF FIRST MIDDLE ARRAY IS GREATER THAN THE MIDDLE INDEX OF SECOND MIDDLE ARRAY, REDEFINE "CURRENT" WITH ZEROTH INDEX VALUE OF THE CURRENT MIDDLE SQUARES' INDEX => 1; 
-            // ELSE IF MIDDLE INDEX OF SECOND MIDDLE ARRAY IS GREATER THAN THE MIDDLE INDEX OF THE FIRST MIDDLE ARRAY, REDEFINE "CURRENT" WITH FIRST INDEX VALUE OF THE CURRENT MIDDLE SQUARES' INDEX => 2
-        current = currentMidSquares[0][mid] > currentMidSquares[[1][mid]] ? [0, [mid]] : [1, mid]
-        
-        // DEFINE VARIABLES FOR LEFT AND RIGHT OF CURRENT VALUES; FOR READABILITY
-        let left = [current[0], current[1]-1];
-        let right = [current[0], current[1]+1];
-
-        // CHECK IF LEFT OR RIGHT ELEMENTS OF CURRENT INDEX HAVE A GREATER ELEMENT VALUE
-        if (mtx[left[0]][left[1]] > mtx[current[0][current[1]]]) current = [left[0]][left[1]];
-        if (mtx[right[0]][right[1]] > mtx[current[0][current[1]]]) current = [right[0]][right[1]];
-
-        console.log(mtx[left[0]][left[1]])
-
         for(col = 0; col < mtx.length; col++) {
-            row = current[0], col = current[1]
+            row = currentIdx[0], col = currentIdx[1]
             let top = [row-1, col];
             left = [row, col-1];
             right = [row, col+1];
             let bottom = [row+1, col];
-
-            // if()
+            let sidesOfCurrent = [mtx[top[0]][[top[1]]], mtx[left[0]][[left[1]]], mtx[right[0]][[right[1]]], mtx[bottom[0]][[bottom[1]]]]
+            // console.log(sidesOfCurrent)
+            console.log(Math.max(...sidesOfCurrent))
         }
+
+        
     }
 
-    // return carrotsEaten;
+    return carrotsEaten;
 }
 
 console.log(carrotGarden(garden));
