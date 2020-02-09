75c6b75655450fde372f33caa423ef9ed3612d2c
pixelynx <tjstevens1992@gmail.com>
2020-02-08 21:12:16 -0500

    found Pi :3

---
+++ changePiRecursive.jsx
@@ -0,0 +1,17 @@
+// Given a string, compute recursively (no loops) a new string where all appearances of "pi" have been replaced by "3.14".
+
+const changePi = (str) => {
+    if(!str.length) return "";
+    let firstChar = str[0], secChar = str[1];
+
+    if(firstChar === "p" && secChar === "i") {
+        let foundPi = "3.14";
+        return foundPi += changePi(str.slice(2))
+    } else {
+        return firstChar += changePi(str.slice(1))
+    }
+}
+
+console.log(changePi("xpix")) // "x3.14x"
+console.log(changePi("pipi")) // "3.143.14"
+console.log(changePi("pip")) // "3.14p"
