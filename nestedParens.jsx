bdea5aa53b9a53bcc643cf17c22f9d6afbdb980f
pixelynx <tjstevens1992@gmail.com>
2020-02-08 20:51:28 -0500

    can check nested parens recursively!

---
+++ nestedParens.jsx
@@ -0,0 +1,16 @@
+// Given a string, return true if it is a nesting of zero or more pairs of parenthesis, like "(())" or "((()))". Suggestion: check the first and last chars, and then recur on what's inside them.
+
+const nestedParens = (str) => {
+    if(!str.length) return true;
+    let startChar = str.slice(0, 1);
+    let lastChar = str.slice(str.length-1, str.length);
+    str = str.slice(1, str.length-1);
+
+    if(startChar === "(" && lastChar === ")") {
+        return nestedParens(str);
+    } else return false;
+}
+
+console.log(nestedParens("(())")) // true
+console.log(nestedParens("((()))")) // true
+console.log(nestedParens("(((x))")) // false
