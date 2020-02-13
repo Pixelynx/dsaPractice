f86daf98dcd5aeed3f5347ba9b0df8e4a8146fd0
pixelynx <tjstevens1992@gmail.com>
2020-02-12 21:22:39 -0500

    scrapping set

--- superStack.jsx
+++ superStack.jsx
@@ -27,42 +27,17 @@ class Stack{
 
 function superStack(operations) {
     let stack = new Stack();
-
-    // operations.forEach(op => {
-        //     op.split(/(\s+)/)
-        // })
-        
-        
         for(let i = 0; i < operations.length; i++) {
-            let set = new Set(operations[i].split(/(\s+)/))
-            set.delete(set.has(' '))
-            console.log(set)
-        // operations[i].split(/(\s+)/)
-        // switch(set) {
-        //     case set.has('push'):
-        //         console.log('NEXT', first)
-        //         let values = set.values()
-        //         let first = values.next()
-        //         let value = operations[i].pop()
-        //         // console.log(value)
-        //         stack.push(value)
-                
-        //         // console.log(stack.print());
-        //         case "pop":
-        //             stack.pop()
-        //             // console.log(stack.print());
-        // }
-        // return this;
+            // /(\s+)/ 
+            
+            
+            
+        // console.log(stack)
     }
+
 }
 
 let stack = new Stack();
-// stack.push(2)
-// stack.push(3)
-// stack.push(5)
-// stack.pop()
-// stack.push(7)
-// stack.print()
 
 console.log(superStack([ 'push 4', 'pop','push 3','push 5','push 2','inc 3 1','pop','push 1','inc 2 2','push 4','pop','pop' ]))
 
