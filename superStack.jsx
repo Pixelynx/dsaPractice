5aaf132aeca0ec0d90647e2a97f4166b656efece
pixelynx <tjstevens1992@gmail.com>
2020-02-15 22:41:00 -0500

    slightly improved

--- superStack.jsx
+++ superStack.jsx
@@ -5,6 +5,7 @@ class Stack{
 
     push(v) {
         this.items.push(v);
+        return this;
     }
     
     pop() {
@@ -14,7 +15,7 @@ class Stack{
     inc(i, v) {
         let items = this.items;
         let count = 0;
-        while(count <= i) {
+        while(count < i) {
             items[count] = v + items[count]
             count++
         }
@@ -22,7 +23,7 @@ class Stack{
     }
 
     print() {
-        return !this.items.length ? "EMPTY" : this.items[0];
+        return !this.items.length ? "EMPTY" : this.items[this.items.length-1];
     }
 }
 
@@ -32,22 +33,37 @@ function superStack(operations) {
         for(let i = 0; i < operations.length; i++) {
             let currentItem = operations[i].split(' ');
 
-            if(currentItem[0] === 'push') {
-                currentItem[1] = Number(currentItem[1]);
-                stack.push(currentItem[1])
-                console.log(stack.print());
-            }
-            if(currentItem[0] === 'pop') {
-                stack.pop();
-                console.log(stack.print());
-            }
-            if(currentItem[0] === 'inc') {
-                currentItem[1] = Number(currentItem[1]), currentItem[2] = Number(currentItem[2]);;
+            switch(currentItem[0]) {
+                case "push":
+                    currentItem[1] = Number(currentItem[1]);
+                    stack.push(currentItem[1])
+                    console.log(stack.print());
+                    break;
+                case "pop":
+                    stack.pop();
+                    console.log(stack.print());
+                    break;
+                case "inc":
+                    currentItem[1] = Number(currentItem[1]), currentItem[2] = Number(currentItem[2]);
                 stack.inc(currentItem[1], currentItem[2])
                 console.log(stack.print());
-            }  
+            }   
+
+            // if(currentItem[0] === 'push') {
+            //     currentItem[1] = Number(currentItem[1]);
+            //     stack.push(currentItem[1])
+            //     console.log(stack.print());
+            // }
+            // if(currentItem[0] === 'pop') {
+            //     stack.pop();
+            //     console.log(stack.print());
+            // }
+            // if(currentItem[0] === 'inc') {
+            //     currentItem[1] = Number(currentItem[1]), currentItem[2] = Number(currentItem[2]);
+            //     stack.inc(currentItem[1], currentItem[2])
+            //     console.log(stack.print());
+            // }  
     }
-    return this;
 }
 
 console.log(superStack([ 'push 4', 'pop','push 3','push 5','push 2','inc 3 1','pop','push 1','inc 2 2','push 4','pop','pop' ]))
@@ -58,33 +74,33 @@ console.log(superStack([ 'push 4', 'pop','push 3','push 5','push 2','inc 3 1','p
 // 3
 // => 3 
 // 3 5
-// => 3 
+// => 5 
 // 3 5 2
-// => 3
+// => 2
 // 4 6 3
-// => 4
+// => 3
 // 4 6
-// => 4
+// => 6
 // 4 6 1
+// => 1
+// 6 8 1
+// => 1
+// 6 8 1 4
 // => 4
+// 6 8 1
+// => 1
 // 6 8
-// => 6
-// 6 8 4
-// => 6
-// 6 8
-// => 6
-// 6
-// => 6
+// => 8
 
 // 4
 // EMPTY
 // 3
+// 5
+// 2
 // 3
-// 3
+// 6
+// 1
+// 1
 // 4
-// 4
-// 4
-// 6
-// 6
-// 6
-// 6
+// 1
+// 8
