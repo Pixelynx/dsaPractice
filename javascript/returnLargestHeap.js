0a034c1b1542595d5b8903489de44c4d5196e5e3
pixelynx <tjstevens1992@gmail.com>
2020-02-19 17:55:37 -0500

    min heap inserts and sorts. max heap bugged

---
+++ javascript/returnLargestHeap.js
@@ -0,0 +1,164 @@
+// Here is the question in a more formal way:
+// You have K machines/server each of which is connected to 1 central machine. Each of the K machines have an array of sorted files.  File size is not unique. Find the K largest file across all K machines. THIS WILL USE A HEAP!
+
+/* Heaps */
+
+// left child: i * 2
+// right child: i * 2 + 1
+// parent: i / 2
+
+class MinHeap {
+	constructor() {
+	    this.heap = [null];
+    }
+    
+	insert(num) {
+        let heap = this.heap;
+        
+        heap.push(num);
+		if(heap.length) {
+            let idx = heap.length - 1;
+            let current = heap[idx];
+            
+            if(heap[0] === null) heap.shift();
+            
+			while(current < heap[idx - 1]) {
+				if(idx >= 1) {
+					[heap[idx - 1], current] = [current, heap[idx - 1]];
+					if(Math.floor(idx/2) > 1) {
+                        idx = idx-1;
+					} else {
+						break;
+                    };
+				};
+			};
+        };
+        if(heap[0] > heap[1]) [heap[0], heap[1]] = [heap[1], heap[0]];
+	};
+	
+	remove() {
+        let heap = this.heap;
+        let smallest = heap[1];
+        
+		if (heap.length > 2) {
+			heap[1] = heap[heap.length - 1];
+			heap.splice(heap.length - 1);
+			if (heap.length == 3) {
+				if (heap[1] > heap[2]) {
+					[heap[1], heap[2]] = [heap[2], heap[1]];
+				};
+				return smallest;
+			};
+			let i = 1;
+			let left = 2 * i;
+			let right = 2 * i + 1;
+			while (heap[i] >= heap[left] || heap[i] >= heap[right]) {
+				if (heap[left] < heap[right]) {
+					[heap[i], heap[left]] = [heap[left], heap[i]];
+					i = 2 * i
+				} else {
+					[heap[i], heap[right]] = [heap[right], heap[i]];
+					i = 2 * i + 1;
+				};
+				left = 2 * i;
+				right = 2 * i + 1;
+				if (heap[left] == undefined || heap[right] == undefined) {
+					break;
+				};
+			};
+		} else if (heap.length == 2) {
+			heap.splice(1, 1);
+		} else {
+			return null;
+		};
+		return smallest;
+	};
+  
+	sort() {
+		let result = new Array();
+		while (this.heap.length > 1) {
+			result.push(this.remove());
+		};
+		return this.heap = result;
+	};
+
+};
+
+class MaxHeap {
+	constructor() {
+	    this.heap = [null];
+    }
+    
+	insert(num) {
+        let heap = this.heap;
+        
+        heap.push(num);
+		if(heap.length) {
+            let idx = heap.length - 1;
+            let current = heap[idx];
+            
+            if(heap[0] === null) heap.shift();
+            
+			while(current > heap[idx - 1]) {
+				if(idx >= 1) {
+					[heap[idx - 1], current] = [current, heap[idx - 1]];
+					if(Math.floor(idx/2) > 1) {
+                        idx = idx-1;
+					} else {
+						break;
+                    };
+				};
+			};
+        };
+        if(heap[0] < heap[1]) [heap[0], heap[1]] = [heap[1], heap[0]];
+	};
+	
+	remove() {
+        let heap = this.heap;
+        let smallest = heap[1];
+        
+		if (heap.length > 2) {
+			heap[1] = heap[heap.length - 1];
+			heap.splice(heap.length - 1);
+			if (heap.length == 3) {
+				if (heap[1] < heap[2]) {
+					[heap[1], heap[2]] = [heap[2], heap[1]];
+				};
+				return smallest;
+			};
+			let i = 1;
+			let left = 2 * i;
+			let right = 2 * i + 1;
+			while (heap[i] <= heap[left] || heap[i] <= heap[right]) {
+				if (heap[left] > heap[right]) {
+					[heap[i], heap[left]] = [heap[left], heap[i]];
+					i = 2 * i
+				} else {
+					[heap[i], heap[right]] = [heap[right], heap[i]];
+					i = 2 * i + 1;
+				};
+				left = 2 * i;
+				right = 2 * i + 1;
+				if (heap[left] == undefined || heap[right] == undefined) {
+					break;
+				};
+			};
+		} else if (heap.length == 2) {
+			heap.splice(1, 1);
+		} else {
+			return null;
+		};
+		return smallest;
+	};
+
+};
+
+let newHeap = new MaxHeap();
+newHeap.insert(10)
+newHeap.insert(5)
+newHeap.insert(1)
+newHeap.insert(12)
+newHeap.insert(9)
+newHeap.insert(6)
+// newHeap.sort()
+console.log(newHeap)
