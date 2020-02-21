6b1ffea9dfa9509cd62cabcf864c048469937e94
pixelynx <tjstevens1992@gmail.com>
2020-02-21 01:46:02 -0500

    fail with for of loop

--- javascript/inProg/anagram.js
+++ javascript/inProg/anagram.js
@@ -1,21 +1,67 @@
-const isAnagram = (str1, str2) => {
-    let lets = {};
+// const isAnagram = (str1, str2) => {
+//     let lets = {};
 
-    if(str1.length !== str2.length) return false 
+//     if (str1.length !== str2.length) return false
 
-    for(let i = 0; i < str1.length; i++) {
-        let strChar = str1[i];
-        if(!lets[strChar]) lets[strChar] = 1
+//     for (let letter of str1) {
+//         if (lets[letter]) lets[letter]++;
+//         else lets[letter] = 1;
+//     }
+
+//     // for(let i = 0; i < str1.length; i++) {
+//     //     let strChar = str1[i];
+//     //     if(!lets[strChar]) lets[strChar] = 1
+//     //     else {
+//     //         lets[strChar] = lets[strChar]++
+//     //     }
+//     // }
+
+//     // for(let i = 0; i < str2.length; i++) {
+//     //     let strChar = str2[i];
+//     //     if(lets[strChar]) lets[strChar]--
+//     //     else return false;
+//     // } return true;
+// };
+
+// console.log(isAnagram("listen", "silent"))
+
+
+
+
+const anagram = (str1, str2) => {
+    // if(str1.length !== str2.length){
+    //   return false
+    // }
+    let letters = {}
+    // for(let i = 0; i < str1.length; i++){
+    //   let keyedInLetters = letters[str1[i]] 
+    //   if(!keyedInLetters){
+
+    //     keyedInLetters = 1
+    //   }
+    //   else{
+    //     keyedInLetters = keyedInLetters + 1
+    //   }
+    // }
+
+    for (let letter of str1) {
+        if (letters[letter]) {
+            letters[letter]++
+        }
         else {
-            lets[strChar] = lets[strChar]++
+            letters[letter] = letters[letter] + 1
         }
     }
 
-    for(let i = 0; i < str2.length; i++) {
-        let strChar = str2[i];
-        if(lets[strChar]) lets[strChar]--
-        else return false;
-    } return true;
-};
+    for (let i = 0; i < str2.length; i++) {
+        if (letters[str2[i]]) {
+            letters[str2[i]] = letters[str2[i]] - 1
+        }
+        else {
+            return false
+        }
+    }
+    return true
 
-console.log(isAnagram("listens", "silent"))
+}
+console.log(anagram("listen", "silent"))
