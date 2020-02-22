638a01a6f342d8590042463476b3a3d8daafcf53
pixelynx <tjstevens1992@gmail.com>
2020-02-21 19:33:47 -0500

    add solution to find lowest primes

---
+++ javascript/findLowestPrime.js
@@ -0,0 +1,22 @@
+const findPrime = (num) => {
+    let primes = new Set();
+
+    for(let i = 2; i < num; i++) {
+        let currentVal = num;
+
+        while(currentVal !== 2 && (currentVal%2===0 || currentVal%1===0)) {
+            if((currentVal/i)%2===0 || (currentVal/i)%1===0) {
+                currentVal = currentVal/i;
+            } else {
+                if((currentVal/2)%1===0) {
+                    currentVal = currentVal/2;
+                }
+                else break;
+            }
+        };
+        if(!primes.has(currentVal) && currentVal!==num) primes.add(currentVal);
+    }
+    return primes;
+};
+
+console.log(findPrime(23))
