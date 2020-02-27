a31f819bd439ecc3aafd654f817464b6f876375f
pixelynx <tjstevens1992@gmail.com>
2020-02-26 22:36:50 -0500

    add changes

--- javascript/calculateClockAngles.js
+++ javascript/calculateClockAngles.js
@@ -3,7 +3,7 @@ const calcClockHandAngles = (time) => {
     // when minutes hits 60; reset minutes to 0
 
     time = time.split(":")
-    let hH = time[0];
+    let hH = Number(time[0]);
     let mH = time[1];
     if(mH === 60) mH = 0;
     if(hH > 12) hH = hH - 12;
@@ -11,11 +11,10 @@ const calcClockHandAngles = (time) => {
 
     let mHAngle = mH * 6;
     let hHAngle = hH * 30 + (mH * 0.5);
-    console.log(hHAngle, mHAngle)
+
     let currentAngle = Math.abs(mHAngle - hHAngle);
-    console.log(360 - 72)
+
     return Math.min(currentAngle, 360 - currentAngle);
 };
 
 console.log(calcClockHandAngles("5:37"));
-
