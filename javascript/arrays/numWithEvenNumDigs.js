50980c0e4a6d57689c93a77bc930978181e55b22
pixelynx <tjstevens1992@gmail.com>
2020-03-05 19:26:25 -0500

    solve leetcode problem first correct try

---
+++ javascript/arrays/numWithEvenNumDigs.js
@@ -0,0 +1,17 @@
+// Given an array nums of integers, return how many of them contain an even number of digits.
+ 
+// Example 1:
+// Input: nums = [12,345,2,6,7896]
+// Output: 2
+
+const findNumbers = (nums) => {
+    nums = nums.map(num => num.toString());
+    let evenCount = 0;
+
+    nums.forEach(num => {
+        if(num.length%2 === 0) evenCount++;
+    })
+    return evenCount;
+};
+
+console.log(findNumbers([12,345,2,6,7896]));
