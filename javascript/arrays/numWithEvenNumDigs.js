e71cc584bf565fd5076a9bb180117857765bda52
pixelynx <tjstevens1992@gmail.com>
2020-03-07 22:30:47 -0500

    add num w. even digs one liner

--- javascript/arrays/numWithEvenNumDigs.js
+++ javascript/arrays/numWithEvenNumDigs.js
@@ -4,14 +4,18 @@
 // Input: nums = [12,345,2,6,7896]
 // Output: 2
 
-const findNumbers = (nums) => {
-    nums = nums.map(num => num.toString());
-    let evenCount = 0;
+// const findNumbers = (nums) => {
+//     nums = nums.map(num => num.toString());
+//     let evenCount = 0;
 
-    nums.forEach(num => {
-        if(num.length%2 === 0) evenCount++;
-    })
-    return evenCount;
+//     nums.forEach(num => {
+//         if(num.length%2 === 0) evenCount++;
+//     })
+//     return evenCount;
+// };
+
+const findNumbers = (nums) => {
+    return nums.filter(num => num.toString().length % 2 === 0).length;
 };
 
 console.log(findNumbers([12,345,2,6,7896]));
