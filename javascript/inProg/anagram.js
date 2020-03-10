9328de5ec07328811561893fb4894bda9ddb7112
pixelynx <tjstevens1992@gmail.com>
2020-03-10 00:02:48 -0400

    test

--- javascript/inProg/anagram.js
+++ javascript/inProg/anagram.js
@@ -1,55 +1,17 @@
-// const isAnagram = (str1, str2) => {
-//     let lets = {};
-
-//     if (str1.length !== str2.length) return false
-
-//     for (let letter of str1) {
-//         if (lets[letter]) lets[letter]++;
-//         else lets[letter] = 1;
-//     }
-
-//     // for(let i = 0; i < str1.length; i++) {
-//     //     let strChar = str1[i];
-//     //     if(!lets[strChar]) lets[strChar] = 1
-//     //     else {
-//     //         lets[strChar] = lets[strChar]++
-//     //     }
-//     // }
-
-//     // for(let i = 0; i < str2.length; i++) {
-//     //     let strChar = str2[i];
-//     //     if(lets[strChar]) lets[strChar]--
-//     //     else return false;
-//     // } return true;
-// };
-
-// console.log(isAnagram("listen", "silent"))
-
-
-
-
 const anagram = (str1, str2) => {
     // if(str1.length !== str2.length){
     //   return false
     // }
     let letters = {}
-    // for(let i = 0; i < str1.length; i++){
-    //   let keyedInLetters = letters[str1[i]] 
-    //   if(!keyedInLetters){
-
-    //     keyedInLetters = 1
-    //   }
-    //   else{
-    //     keyedInLetters = keyedInLetters + 1
-    //   }
-    // }
 
     for (let letter of str1) {
         if (letters[letter]) {
+            console.log("IF: ", letters)
             letters[letter]++
         }
         else {
-            letters[letter] = letters[letter] + 1
+            console.log("ELIF: ", letters)
+            letters[letter]++
         }
     }
 
