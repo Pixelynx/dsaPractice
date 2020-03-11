893ae90812f0c333cf8e1105c63a4dc694b15149
pixelynx <tjstevens1992@gmail.com>
2020-03-11 19:55:17 -0400

    add recursive solution

--- javascript/inProg/anagram.js
+++ javascript/inProg/anagram.js
@@ -1,19 +1,32 @@
-const anagram = (str1, str2) => {
-    if(str1.length !== str2.length){
-      return false
-    }
-    let letters = {}
+// const anagram = (str1, str2) => {
+//     if(str1.length !== str2.length){
+//       return false
+//     }
+//     let letters = {}
 
-    for(let letter of str1) {
-            letters[letter] = letters[letter]+1 || 1
-        }
+//     for(let letter of str1) {
+//             letters[letter] = letters[letter]+1 || 1
+//         }
         
-        for(let letter of str2) {
-            if(letters[letter] && letters[letter] > 0) {
-                letters[letter]--
-                console.log("IF: ", letters)
-        } else return false
-    } 
-    return true;
+//         for(let letter of str2) {
+//             if(letters[letter] && letters[letter] > 0) {
+//                 letters[letter]--
+//         } else return false
+//     } 
+//     return true;
+// }
+
+const anagram = (str1, str2) => {
+    if(str1.length !== str2.length) return false;
+    if(!str1.length && !str2.length) return true;
+    str1 = str1.split("").sort();
+    str2 = str2.split("").sort();
+
+    if(str1.shift() === str2.shift()) {
+        str1 = str1.join(""), str2 = str2.join("");
+        return anagram(str1, str2);
+    }
+    return false;
 }
-console.log(anagram("listens", "silentd"))
+
+console.log(anagram("listed", "silent"))
