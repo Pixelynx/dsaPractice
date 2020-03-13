061e4d806e2e0f6bdd2b430605b7889502b4162a
pixelynx <tjstevens1992@gmail.com>
2020-03-13 00:18:38 -0400

    solve using reduce

--- javascript/inProg/subtractProductSum.js
+++ javascript/inProg/subtractProductSum.js
@@ -8,16 +8,30 @@
 // Sum of digits = 2 + 3 + 4 = 9 
 // Result = 24 - 9 = 15
 
-const subtractProductAndSum = (n) => {
-    let prod = 1, sum = 0;
-    n = n.toString().split("");
-    n = n.map(num => num = Number(num));
+// const subtractProductAndSum = (n) => {
+//     let prod = 1, sum = 0;
+//     n = n.toString().split("");
+//     n = n.map(num => num = Number(num));
 
-    for(let i = 0; i < n.length; i++) {
-        prod = prod * n[i];
-        sum = sum + n[i];
-    }
-    return Math.max(prod-sum);
+//     for(let i = 0; i < n.length; i++) {
+//         prod = prod * n[i];
+//         sum = sum + n[i];
+//     }
+//     return Math.max(prod-sum);
+// };
+
+// const subtractProductAndSum = (n) => {
+//     n = n.toString().split("");
+//     n = n.map(num => num = Number(num));
+
+//     let prod = n.reduce((n1, n2) => n1*n2);
+//     let sum = n.reduce((n1, n2) => n1+n2);
+
+//     return Math.max(prod-sum);
+// }
+
+const subtractProductAndSumRecurse = (n) => {
+
 };
 
 console.log(subtractProductAndSum(234));
