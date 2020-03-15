1b6e1a0ec78f58a5fc229a3f76f033776c07bd5b
pixelynx <tjstevens1992@gmail.com>
2020-03-15 01:56:01 -0400

    create initial graph logic

--- javascript/arrays/cats.js
+++ javascript/arrays/cats.js
@@ -13,11 +13,56 @@ let projects = [
     "Handyman"
 ];
 
-const categorySuggestions = (categories, projects, k) => {
+class CategoryGraph {
+    constructor(numOfVerticies) {
+        this.numOfVerticies = numOfVerticies;
+        this.edges = new Map();
+    }
 
+    addVertex = (val) => {
+        this.edges.set(val, []);
+    };
+
+    addEdge = (v1, v2) => {
+        this.edges.get(v1).push(v2);
+        this.edges.get(v2).push(v1);
+
+        return this;
+    };
+
+    print = () => {
+        let getKeys = this.edges.keys();
+
+        for(let keyVal of getKeys) {
+            let getVals = this.edges.get(keyVal);
+            let disp = "";
+
+            for(let val of getVals) {
+                disp += val + " " 
+            };
+
+            return `${keyVal} => disp`;
+        };
+    }
 }
 
-console.log(categorySuggestions, projects, 3);
+let verticies = ["House Painting", "Interior Painting", "Handyman", "Massage Therapy", "House Cleaning", "Furniture Assembly", "Plumbing Drain Repair", "Junk Removal"];
+
+let newGraph = new CategoryGraph();
+
+for(let i = 0; i < verticies.length; i++) {
+    newGraph.addVertex(verticies[i]);
+}
+newGraph.addEdge("House Painting", "Interior Painting").addEdge("Handyman", "Massage Therapy").addEdge("House Painting", "House Cleaning").addEdge("Furniture Assembly", "Handyman").addEdge("Furniture Assembly", "Massage Therapy").addEdge("Plumbing Drain Repair", "Junk Removal");
+newGraph.print()
+
+console.log(newGraph)
+
+const categorySuggestions = (categories, projects, k) => {
+
+};
+
+// console.log(categorySuggestions, projects, 3);
 // output => [
 //     ["House Painting", "Interior Painting", "House Cleaning"],
 //     ["Handyman", "House Painting", "Interior Painting"]
