3325db7b586ea4ee5f4cb294088967b5d324e01c
pixelynx <tjstevens1992@gmail.com>
2020-03-16 15:35:44 -0400

    add helpers

--- javascript/arrays/bestValues.js
+++ javascript/arrays/bestValues.js
@@ -2,23 +2,31 @@ let pros = [[5, 4], [4, 3], [6, 5], [3, 5]];
 // => [15, 6, 0, 15]
 // => [0, 3, 1]
 
+function reduceToDistances(arr) {
+    return arr.reduce((distances, pro) => {
+        distances.push(pro[0])
+        return distances;
+    }, new Array());
+};
+
+function calculatePMS(arr, int) {
+    let pmsObj = new Object();
+
+    // equation => (max_distance - distance) * rating
+    for(let pro = 0; pro < arr.length; pro++) {
+        let distance = 0, rating = 1;
+        let PMS = (int - arr[pro][distance]) * arr[pro][rating];
+        pmsObj[pro] = PMS;
+    };
+    return pmsObj;
+};
+
 function bestPros(pros, k) {
-// new array of all the distances
-let distanceArr = pros.reduce((distances, pro) => {
-    distances.push(pro[0])
-    return distances;
-}, []);
+let max_distance = reduceToDistances(pros);
+max_distance = Math.max(...max_distance);
 
-let max_distance = Math.max(...distanceArr);
-let scores = {};
+let scores = calculatePMS(pros, max_distance);
 let kPros = new Set();
-
-// equation => (max_distance - distance) * rating
-for(let pro = 0; pro < pros.length; pro++) {
-    let distance = 0, rating = 1;
-    let PMS = (max_distance - pros[pro][distance]) * pros[pro][rating];
-    (scores[pro]) = PMS
-}
 
 let PMSarr = Object.values(scores).sort((a,b) => b-a).slice(0, k);
 
@@ -28,9 +36,42 @@ for(let rating of PMSarr) {
             kPros.add(parseInt(idx))
         }
     }
-}
+};
+
 return Array.from(kPros);
 };
 
 
 console.log(bestPros(pros, 2));
+
+
+
+// function bestPros(pros, k) {
+// // new array of all the distances
+// let distanceArr = pros.reduce((distances, pro) => {
+//     distances.push(pro[0])
+//     return distances;
+// }, new Array());
+
+// let max_distance = Math.max(...distanceArr);
+// let scores = new Object();
+// let kPros = new Set();
+
+// // equation => (max_distance - distance) * rating
+// for(let pro = 0; pro < pros.length; pro++) {
+//     let distance = 0, rating = 1;
+//     let PMS = (max_distance - pros[pro][distance]) * pros[pro][rating];
+//     (scores[pro]) = PMS
+// }
+
+// let PMSarr = Object.values(scores).sort((a,b) => b-a).slice(0, k);
+
+// for(let rating of PMSarr) {
+//     for(let idx in scores) {
+//         if(!kPros.has(scores[idx]) && scores[idx] === rating) {
+//             kPros.add(parseInt(idx))
+//         }
+//     }
+// }
+// return Array.from(kPros);
+// };
