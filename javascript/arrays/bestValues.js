a133f085c624186790399a0d7631d21ea3233021
pixelynx <tjstevens1992@gmail.com>
2020-03-15 22:55:43 -0400

    refactor code to make helper

--- javascript/arrays/bestValues.js
+++ javascript/arrays/bestValues.js
@@ -1,6 +1,15 @@
 let pros = [[5, 4], [4, 3], [6, 5], [3, 5]];
 // => [0, 3]
 
+const calculateScore = (arr, obj, maxDistance) => {
+    // equation => (max_distance - distance) * rating
+    for(let pro = 0; pro < arr.length; pro++) {
+        let distance = 0, rating = 1;
+        let PMS = (maxDistance - arr[pro][distance]) * arr[pro][rating];
+        (obj[pro]) = PMS
+    };
+}
+
 const bestPros = (pros, k) => {
     // new array of all the distances
     let max_distance = pros.reduce((distances, pro) => {
@@ -8,18 +17,12 @@ const bestPros = (pros, k) => {
         return distances.sort((a, b) => a-b).slice(-1);
     }, new Array());
     
-    let scores = {};
-    let kPros = new Set();
-    
-    // equation => (max_distance - distance) * rating
-    for(let pro = 0; pro < pros.length; pro++) {
-        let distance = 0, rating = 1;
-        let PMS = (max_distance - pros[pro][distance]) * pros[pro][rating];
-        (scores[pro]) = PMS
-    }
+    let scores = new Object();
+    calculateScore(pros, scores, max_distance);
     
     let PMSarr = Object.values(scores).sort((a,b) => b-a).slice(0, k);
-
+    
+    let kPros = new Set();
     // for(let rating of PMSarr) {
     //     for(let idx in scores) {
     //         if(!kPros.has(scores[idx]) && scores[idx] === rating) {
@@ -34,8 +37,6 @@ const bestPros = (pros, k) => {
             kPros.add(parseInt(pRating))
         }
     }
-    console.log(scores)
-    console.log(PMSarr)
     return Array.from((kPros));
 }
 
