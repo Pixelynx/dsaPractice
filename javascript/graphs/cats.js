b755d74688df8e77f6f9f404d0139e69f88df8cf
pixelynx <tjstevens1992@gmail.com>
2020-03-18 23:19:39 -0400

    remove reduntant lines

--- javascript/graphs/cats.js
+++ javascript/graphs/cats.js
@@ -50,16 +50,6 @@ class CategoryGraph {
     }
 }
 
-// let verticies = ["House Painting", "Interior Painting", "Handyman", "Massage Therapy", "House Cleaning", "Furniture Assembly", "Plumbing Drain Repair", "Junk Removal"];
-
-// let newGraph = new CategoryGraph();
-
-// for(let i = 0; i < verticies.length; i++) {
-//     newGraph.addVertex(verticies[i]);
-// }
-// newGraph.addEdge("House Painting", "Interior Painting").addEdge("Handyman", "Massage Therapy").addEdge("House Painting", "House Cleaning").addEdge("Furniture Assembly", "Handyman").addEdge("Furniture Assembly", "Massage Therapy").addEdge("Plumbing Drain Repair", "Junk Removal");
-// newGraph.print()
-
 function createVerticies(graph, arr) {
     let setArr = new Set();
     // create set of unique elements and add them as a vertex to graph
@@ -78,6 +68,7 @@ function splitPairs(arr) {
 function categorySuggestions(categories, projects, k) {
     // split category pairs and order by relevance
     categories = splitPairs(categories).sort((a, b) => b[2]-a[2]);
+    console.log("CATEGORIES: ", categories)
     let justCategories = categories.map(cat => cat.slice(0,-1));
 
     // create graph vertices and add their corresponding edges
@@ -98,6 +89,7 @@ function categorySuggestions(categories, projects, k) {
         let relatedProjects = categoryGraph.getEdges(currentProject.toString()).slice(0, k-1)
         kProjects[i].push(...relatedProjects);
     };
+    console.log(categoryGraph)
     return kProjects;
 };
 
@@ -112,20 +104,5 @@ console.log(categorySuggestions(categories, projects, 3));
 // House Painting | House Cleaning => 0.6
 
 // Handyman | Handyman => 1.0
-// Handyman | House Painting => 0.5
-// Handyman | Interior Painting => 0.0
-
-// The first expected output for the House Painting project seems just fine. However it looks like there is an error when it comes to the Handyman project. Based off the info that is given, I believe the proper output should show ["Handyman", "Funiture Assembly", "House Painting"]. This is because "Funiture Assembly has a relevance rating of 0.8 and "House Painting" has a relevance of 0.5 as per the input that is given, as opposed to the "expected" output which is given of "House Paiting" which is at 0.5 and "Interior Painting" at 0.0:
-
-//     "House Painting, Interior Painting, 0.9"
-//     "Handyman, Massage Therapy, 0.1"
-//     "Handyman, House Painting, 0.5"
-//     "House Painting, House Cleaning, 0.6"
-//     "Furniture Assembly, Handyman, 0.8"
-//     "Furniture Assembly, Massage Therapy, 0.1"
-//     "Plumbing Drain Repair, Junk Removal, 0.3"
-
-//  This example problem is test case 0, and there seems to be a similar bug with test case one. I can't help worry that there are other test cases with the same bug and that makes it difficult to know what can be improved upon in the code.
-
-
-
+// Handyman | House Painting => 1.0
+// Handyman | Interior Painting => 0.9
