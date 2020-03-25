3b10e60bee5aa9708b131458797644409e143732
pixelynx <tjstevens1992@gmail.com>
2020-03-25 16:37:52 -0400

    add logs

---
+++ javascript/arrays/pairAddsUp.js
@@ -0,0 +1,9 @@
+const pairAddsUp = (arr) => {
+
+}
+
+console.log(pairAddsUp([1,3,5,4,2], 8));
+console.log("EXPECTED OUTPUT: ", true);
+console.log("_____");
+console.log(pairAddsUp([1,3,5,4,2], 2));
+console.log("EXPECTED OUTPUT: ", false);
