05938551e368c93595e8a35e176da1bbf86c8e8b
pixelynx <tjstevens1992@gmail.com>
2020-03-25 00:52:34 -0400

    add recursion

--- javascript/arrays/doesNotContainNum.js
+++ javascript/arrays/doesNotContainNum.js
@@ -1,7 +1,21 @@
+let log = console.log;
 const intRange = (n1, n2) => {
-    for(let num = n1; num < n2; num++) {
-        console.log(num)
-    }
-}
+    function checkNumContains5(n) {
+        n = n.toString().split("");
+        if(!n.length) return true;
 
-console.log(intRange(2, 26));
+        let currNum = n.slice(0, 1);
+        if(parseFloat(currNum) === 5) return false;
+        else {
+            n = n.slice(1).join("");
+            return checkNumContains5(n);
+        };
+    };
+
+    for(let num = n1; num < n2; num++) {
+        if(num !== 5 && checkNumContains5(num) === true) log(num);
+    };
+    
+};
+
+console.log(intRange(2, 56));
