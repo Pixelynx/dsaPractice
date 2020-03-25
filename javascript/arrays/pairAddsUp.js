9b140dce89bd727c16f26fff61842a2f6514207c
pixelynx <tjstevens1992@gmail.com>
2020-03-25 19:19:22 -0400

    remove log

--- javascript/arrays/pairAddsUp.js
+++ javascript/arrays/pairAddsUp.js
@@ -6,21 +6,19 @@ const pairAddsUp = (arr, target) => {
     arr = arr.sort((a, b) =>  a-b);
 
     function diffMakesTarget(num, target, arr) {
-        console.log(arr[0] + num, target, arr)
         if(!arr.length) return false;
         if((arr[0] !== num) && (arr[0] + num === target)) return true;
         else return diffMakesTarget(num, target, arr.slice(1))
-    }
+    };
 
     for(let num of arr) {
         if(diffMakesTarget(num, target, arr) === true) return true;
-    }
+    };
 
     return false;
 };
 
 console.log(pairAddsUp([1,3,5,4,2], 8));
-// 1, 2, 3, 4, 5
 console.log("EXPECTED OUTPUT: ", true);
 console.log("_____");
 console.log(pairAddsUp([1,3,5,4,2], 2));
