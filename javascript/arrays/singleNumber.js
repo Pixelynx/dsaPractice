d0a615d5f31ee73902261e2d5c445af908c5bd14
pixelynx <tjstevens1992@gmail.com>
2020-04-02 22:38:08 -0400

    set up for problem

---
+++ javascript/arrays/singleNumber.js
@@ -0,0 +1,11 @@
+let singleNumber = function(nums) {
+    
+};
+
+
+let test1 = [2, 2, 1];
+let test2 = [4, 1, 2, 1, 2];
+console.log("OUTPUT 1: ", singleNumber(test1));
+console.log("Expected output 1.");
+console.log("OUTPUT 2: ", singleNumber(test2));
+console.log("Expected output 4.");
