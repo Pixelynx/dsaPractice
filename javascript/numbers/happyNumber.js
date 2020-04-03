705d9f5c0d2a9d398901eed2d24ff1d18c932a2a
pixelynx <tjstevens1992@gmail.com>
2020-04-03 03:58:47 -0400

    fail

--- javascript/numbers/happyNumber.js
+++ javascript/numbers/happyNumber.js
@@ -3,10 +3,18 @@
 // A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
 
 let isHappy = function(n) {
-    
+    n = n.toString().split("");
+    console.log(parseInt(Math.pow(n.shift(), 2)))
+    console.log(n)
+    if(!n.length) return 0;
+    if(n.length > 0) {
+        return parseInt(Math.pow(n.shift(), 2)) + isHappy(n);
+    }
+    // return n
+    // return n === 1 ? true : false;
 };
 
-console.log("OUTPUT: ", isHappy(19));
+console.log("OUTPUT: ", isHappy(192));
 console.log("EXPECTED OUTPUT: TRUE")
 
 // Input: 19
