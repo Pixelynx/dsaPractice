be7948252400e89f065c637447dfaa47a8ff80d6
pixelynx <tjstevens1992@gmail.com>
2020-04-04 19:51:45 -0400

    change variable name

--- javascript/arrays/moveZeros.js
+++ javascript/arrays/moveZeros.js
@@ -1,22 +1,32 @@
 let moveZeroes = function(nums) {
-    let pointerA = 0, pointerB = 1;
+    let zeroPointer = 0, nonZeroPointer = 1;
 
-        if(nums.length === 2 && (nums[pointerA] === 0 && nums[pointerB] !== 0)) [nums[pointerA], nums[pointerB]] = [nums[pointerB], nums[pointerA]];
+        if(nums.length === 2 && (nums[nonZeroPointer] === 0 && nums[zeroPointer] !== 0)) [nums[nonZeroPointer], nums[zeroPointer]] = [nums[zeroPointer], nums[nonZeroPointer]];
       
         if(nums.length > 2) {
-            while((pointerA !== pointerB) && (pointerA && pointerB) !== null) {
-                if(nums[pointerA] === 0 && nums[pointerB] !== 0) pointerB = pointerA, pointerA++;
-                else if((nums[pointerA] !== 0 && nums[pointerB] === 0)) {
-                    [nums[pointerA], nums[pointerB]] = [nums[pointerB], nums[pointerA]];
-                    pointerA++, pointerB++;
-                } else if((nums[pointerA] && nums[pointerB]) === 0) pointerA++;
+            while(((nonZeroPointer || zeroPointer) !== nums.length) && ((nums[nonZeroPointer] && nums[zeroPointer]) !== null)) {
+                console.log(nonZeroPointer, zeroPointer)
+                // if(nonZeroPointer === zeroPointer) nonZeroPointer++;
+                if(nums[nonZeroPointer] === 0 && nums[zeroPointer] !== 0) {
+                    [nums[nonZeroPointer], nums[zeroPointer]] = [nums[zeroPointer], nums[nonZeroPointer]];
+                    [nonZeroPointer, zeroPointer] = [zeroPointer, nonZeroPointer];
+                    nonZeroPointer++, zeroPointer++;
+                } 
+                else if(nums[nonZeroPointer] !== 0 && nums[zeroPointer] === 0) {
+                    if(nonZeroPointer > zeroPointer){
+                        [nums[nonZeroPointer], nums[zeroPointer]] = [nums[zeroPointer], nums[nonZeroPointer]];
+                    }
+                    nonZeroPointer++, zeroPointer++;
+                } else if(nums[nonZeroPointer] === 0 && nums[zeroPointer] === 0) nonZeroPointer++;
             }
         }
     return nums;
 };
 
-let arr = [1,0,0];
-// [1, 0]
+
+let arr = [0, 0, 1];
+let otherray = [0,1,0,3,12]
+// [0a, 0b, 1]
 
 console.log("OUTPUT: ", moveZeroes(arr));
 console.log("EXPECTED OUTPUT: [1,3,12,0,0]")
