ba0087760bb098ed7165885260e2307c773601cf
pixelynx <tjstevens1992@gmail.com>
2020-04-04 17:08:38 -0400

    find bug in algo

--- javascript/arrays/moveZeros.js
+++ javascript/arrays/moveZeros.js
@@ -1,17 +1,42 @@
 let moveZeroes = function(nums) {
     let pointerA = 0, pointerB;
 
-    while(pointerA !== nums.length-1) {
+    while(pointerA !== nums.length) {
         let temp;
-        if(nums[pointerA] === 0) pointerB = pointerA; 
-        else if(nums[pointerA] !== 0 && nums[pointerB] === 0) {
-            temp = nums[pointerA];
+        console.log(pointerA, pointerB)
+        if(nums[pointerA] === 0) {
+            pointerB = pointerA;
+        } else if(nums[pointerA] !== 0) {
+            temp = nums[pointerB];
+            console.log('temp: ', temp)
+            console.log('nums[A]: ', nums[pointerA])
+            console.log('nums[B]: ', nums[pointerB])
+            
             nums[pointerB] = nums[pointerA];
             nums[pointerA] = temp;
+            console.log('new nums[A]: ', nums[pointerA])
+            console.log('new nums[B]: ', nums[pointerB])
+            console.log('END CYCLE: ', nums)
         }
         pointerA++;
     }
+    return nums
 };
+
+// let moveZeroes = function(nums) {
+//     let pointerA = 0, pointerB;
+
+//     while(pointerA !== nums.length-1) {
+//         let temp;
+//         if(nums[pointerA] === 0) pointerB = pointerA; 
+//         else if(nums[pointerA] !== 0 && nums[pointerB] === 0) {
+//             temp = nums[pointerA];
+//             nums[pointerB] = nums[pointerA];
+//             nums[pointerA] = temp;
+//         }
+//         pointerA++;
+//     }
+// };
 
 let arr = [0,1,0,3,12];
 
