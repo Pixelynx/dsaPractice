9ee2ac0eb61ebf05fca27c504437fed99e8e6151
pixelynx <tjstevens1992@gmail.com>
2020-04-05 23:58:18 -0400

    add solution

--- javascript/numbers/happyNumber.js
+++ javascript/numbers/happyNumber.js
@@ -2,11 +2,22 @@
 
 // A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
 
+// modular 10 on a number returns the last idx
+// divide number by 10 to drop the last idx
+
 let isHappy = function(n) {
-    
+    const nums = new Set();
+    while (!nums.has(n)) {
+      nums.add(n);
+      n = [...`${n}`].reduce((acc, curr) => {
+          return (acc + (curr) ** 2)
+        }, 0);
+      if (n === 1) return true;
+    }
+    return false;
 };
 
-console.log("OUTPUT: ", isHappy(192));
+console.log("OUTPUT: ", isHappy(19));
 console.log("EXPECTED OUTPUT: TRUE")
 
 // Input: 19
