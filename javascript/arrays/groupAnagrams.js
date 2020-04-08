4de1b13e92edf4e8ae9068e2da5c1816195af638
pixelynx <tjstevens1992@gmail.com>
2020-04-07 23:33:03 -0400

    reset thought process

--- javascript/arrays/groupAnagrams.js
+++ javascript/arrays/groupAnagrams.js
@@ -1,17 +1,33 @@
+// let groupAnagrams = function(strs) {
+//     let wordBank = new Object();
+//     let anagramGroups = new Map();
+//     for(word of strs) {
+//         if(!wordBank[word]) wordBank[word] = new Array(word.split("").sort().join(""));
+//         else wordBank[word].push(word)
+//     };
+//     console.log(wordBank)
+//     for(word in wordBank) {
+//         wordBank[word] = wordBank[word].toString();
+//         if(!anagramGroups.has(wordBank[word])) {
+//             anagramGroups.set(wordBank[word], new Array(word))
+//         } else anagramGroups.get(wordBank[word]).push(word);
+//         console.log(anagramGroups)
+//     };
+//     return Array.from(anagramGroups.values());
+// };
+
 let groupAnagrams = function(strs) {
     let wordBank = new Object();
-    let anagramGroups = new Map();
     for(word of strs) {
-        if(!wordBank[word]) wordBank[word] = word.split("").sort().join("");
+        if(!wordBank[word]) wordBank[word] = new Array(word.split("").sort().join(""));
+        else wordBank[word].push(word)
     };
-    for(word in wordBank) {
-        if(!anagramGroups.has(wordBank[word])) {
-            anagramGroups.set(wordBank[word], new Array(word))
-        } else anagramGroups.get(wordBank[word]).push(word);
-    };
-    return Array.from(anagramGroups.values());
+    console.log(wordBank)
 };
 
 let wordBank = ["eat", "tea", "tan", "ate", "nat", "bat"];
+let forFuckSake = ["", ""]
 console.log("OUTPUT: ", groupAnagrams(wordBank));
 console.log("EXPECTED OUTPUT: [[ate,eat,tea],[nat,tan],[bat]]");
+console.log("OUTPUT: ", groupAnagrams(forFuckSake));
+console.log('EXPECTED OUTPUT: [["", ""]]');
