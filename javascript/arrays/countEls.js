7aa77591eb5a2802fba061b4e1c38350d8a6d47e
pixelynx <tjstevens1992@gmail.com>
2020-04-08 22:00:27 -0400

    setup algorithm

---
+++ javascript/arrays/countEls.js
@@ -0,0 +1,23 @@
+let countElements = function(arr) {
+    
+};
+
+let arr1 = [1,2,3];
+console.log("OUTCOME: ", countElements(arr1));
+console.log("EXPECTED OUTPUT: 2");
+// Explanation: 1 and 2 are counted cause 2 and 3 are in arr.
+
+let arr2 = [1,1,3,3,5,5,7,7];
+console.log("OUTCOME: ", countElements(arr2));
+console.log("EXPECTED OUTPUT: 0");
+// Explanation: No numbers are counted, cause there's no 2, 4, 6, or 8 in arr.
+
+let arr3 = [1,3,2,3,5,0];
+console.log("OUTCOME: ", countElements(arr3));
+console.log("EXPECTED OUTPUT: 3");
+// Explanation: 0, 1 and 2 are counted cause 1, 2 and 3 are in arr.
+
+let arr4 = [1,1,2,2];
+console.log("OUTCOME: ", countElements(arr4));
+console.log("EXPECTED OUTPUT: 2");
+// Explanation: Two 1s are counted cause 2 is in arr.
