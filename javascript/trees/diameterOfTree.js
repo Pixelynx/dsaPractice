830bf55f6727d78a9a136d8360824a3c4c65fc14
pixelynx <tjstevens1992@gmail.com>
2020-04-12 00:03:58 -0400

    add full answer

--- javascript/trees/diameterOfTree.js
+++ javascript/trees/diameterOfTree.js
@@ -1,33 +1,51 @@
 class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
     }
 };
 
 class Tree {
     constructor() {
-        this.head = null;
+        this.root = null;
     }
 
-    addNode = (val) => {
-        let node = new Node(val);
-        if(!this.head) this.head = node;
-        else {
-            let current = this.head;
-            while(current.next) {
-                current = current.next;
-            }
-        current.next = node;
+    insertNode = (node, newNode) => {
+        if(newNode.data < node.data) {
+            !node.left ? node.left = newNode : this.insertNode(node.left, newNode);
+        } else {
+            !node.right ? node.right = newNode : this.insertNode(node.right, newNode);
         }
         return this;
+    };
 
+    insert = (data) => {
+        let node = new Node(data);
+        if(!this.root) this.root = node;
+        else this.insertNode(this.root, node);
+        return this;
     };
 };
 
 let newTree = new Tree();
-newTree.addNode(3).addNode(5).addNode(19).addNode(6);
+newTree.insert(1).insert(2).insert(3).insert(4).insert(5);
+console.log(newTree)
 
-let diameterOfBinaryTree = function(root) {
-    
+let diameterOfBinaryTree = function(root, max = 0) {
+    let maxDepth = (root) => {
+        if(!root) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    if(!root) return 0;
+    else {
+        let dist = maxDepth(root.left) + maxDepth(root.right);
+        if(dist > max) max = dist;
+        return Math.max(diameterOfBinaryTree(root.left, max), diameterOfBinaryTree(root.right, max));
+    }   
+
 };
+
+
+console.log(diameterOfBinaryTree(newTree));
