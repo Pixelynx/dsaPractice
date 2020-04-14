b81dd41d218a38813b29d32409ebf41b85be63b7
pixelynx <tjstevens1992@gmail.com>
2020-04-14 01:18:01 -0400

    make pseudo code

--- javascript/arrays/contigiousMaxLen.js
+++ javascript/arrays/contigiousMaxLen.js
@@ -1,21 +1,21 @@
 // Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
 
-let findMaxLength = function(nums) {
+let findMaxLength = function (nums) {
     let count = 0;
     let maxLength = 0;
     let currentLen = 0;
-    
-    for(let start = 0, end = nums.length-1; ((start && maxLength) <= nums.length) && start < end;) {
+
+    for (let start = 0, end = nums.length - 1; ((start && maxLength) <= nums.length) && start < end;) {
         // let start = 0; end = nums.length-1;
-        if(nums[start] === nums[end]) {
+        if (nums[start] === nums[end]) {
             console.log("IF: ", nums)
-            nums = nums.slice(start+1);
+            nums = nums.slice(start + 1);
             start = 0;
-            end = nums.length-1;
+            end = nums.length - 1;
             currentLen = 0;
         } else {
             start++; end--;
-            currentLen = nums.length >= 2 ? currentLen+=2 : currentLen+=1;
+            currentLen = nums.length >= 2 ? currentLen += 2 : currentLen += 1;
             console.log("ELSE: ", nums)
             console.log("ELSE: ", currentLen)
         }
@@ -25,17 +25,42 @@ let findMaxLength = function(nums) {
     return maxLength;
 };
 
-let test1 = [0,1,1];
+let test1 = [0, 1, 1];
 console.log("OUTPUT: ", findMaxLength(test1));
 console.log("EXPECTED OUTPUT: 2");
 // Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
+// obj = {
+//     0: 1
+//     1: 2
+// }
+// maxLength = 2
 
-let test2 = [0,1,0];
+let test2 = [0, 1, 0];
 console.log("OUTPUT: ", findMaxLength(test2));
 console.log("EXPECTED OUTPUT: 2");
 // Explanation: [0, 1] and [1, 0] is the longest contiguous subarray with equal number of 0 and 1.
+// obj = {
+//     0: 2
+//     1: 1
+// }
+// maxLength = 2
 
-let test3 = [0,1,0,0,1,1,0];
+let test3 = [0, 1, 0, 0, 1, 1, 0];
 console.log("OUTPUT: ", findMaxLength(test3));
 console.log("EXPECTED OUTPUT: 6");
 // Explanation: [1,0,0,1,1,0] is the longest contiguous subarray with equal number of 0 and 1.
+// obj = {
+//     0: 4
+//     1: 3
+// }
+// maxLength = 6
+
+let test4 = [0, 0, 0, 1, 1, 1, 0];
+console.log("OUTPUT: ", findMaxLength(test4));
+console.log("EXPECTED OUTPUT: 6");
+// Explanation: [0,0,0,1,1,1] and [0,0,1,1,1,0] is the longest contiguous subarray with equal number of 0 and 1.
+// obj = {
+//     0: 4
+//     1: 3
+//  }
+ // maxLength = 6
