69c86865980b1340ae0db3e8cd93e70a3e6da519
pixelynx <tjstevens1992@gmail.com>
2020-04-15 01:17:26 -0400

    add change

--- javascript/strings/stringShift.js
+++ javascript/strings/stringShift.js
@@ -5,30 +5,40 @@
 // Similarly, a right shift by 1 means remove the last character of s and add it to the beginning.
 // Return the final string after all operations.
 
-let shiftString = function(s, shifting) {
-    let sliced, rightShift, leftShift;
-    s = s.split("")
+// let shiftString = function(s, shifting) {
+//     let sliced, rightShift, leftShift;
+//     s = s.split("")
 
-    if(shifting > 0) {
-        rightShift = s.length-shifting;
-        sliced = s.slice(rightShift)
-        s = sliced.concat(s.slice(0, rightShift)).join("");
-    } else {
-        leftShift = shifting*(-1);
-         sliced = s.slice(0, leftShift);
-         s = s.slice(leftShift).concat(sliced).join("");
-    }
-    return s;
-}
+//     if(shifting > 0) {
+//         rightShift = s.length-shifting;
+//         sliced = s.slice(rightShift)
+//         s = sliced.concat(s.slice(0, rightShift)).join("");
+//     } else {
+//         leftShift = shifting*(-1);
+//          sliced = s.slice(0, leftShift);
+//          s = s.slice(leftShift).concat(sliced).join("");
+//     }
+//     return s;
+// }
+
+// let stringShift = function(s, shift) {
+//     let shifting = 0;
+
+//     for(let i = 0; i < shift.length; i++) {
+//         shift[i][0] === 0 ? shifting = (shifting - shift[i][1])%s.length : shifting = (shifting + shift[i][1])%s.length
+//     }
+//     return shifting !== 0 ? shiftString(s, shifting) : s;
+// };
 
 let stringShift = function(s, shift) {
-    let shifting = 0;
-
-    for(let i = 0; i < shift.length; i++) {
-        shift[i][0] === 0 ? shifting = (shifting - shift[i][1])%s.length : shifting = (shifting + shift[i][1])%s.length
-    }
-    return shifting !== 0 ? shiftString(s, shifting) : s;
-};
+    let shiftCount = 0;
+    shift.forEach(arr => {
+      let [ direction , amount ] = arr;
+      direction ? shiftCount -= amount : shiftCount += amount;
+    });
+    let rotate = shiftCount % s.length;
+    return s.slice(rotate) + s.slice(0,rotate);
+  };
 
 let str1 = "abc", shift1 = [[0,1],[1,2]];
 let str2 = "abcdefg", shift2 = [[1,1],[1,1],[0,2],[1,3]];
