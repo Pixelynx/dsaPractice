d9b316fce67a86face63a89c00bee03d413f6399
pixelynx <tjstevens1992@gmail.com>
2020-04-27 23:32:46 -0400

    set up problem

---
+++ javascript/multiDimenArrays/largestSquare.js
@@ -0,0 +1,14 @@
+// Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+let maximalSquare = function(mtx) {
+
+};
+
+let mtx = [
+    ["1","0","1","0","0"],
+    ["1","0","1","1","1"],
+    ["1","1","1","1","1"],
+    ["1","0","0","1","0"]
+];
+
+console.log(maximalSquare(mtx))
