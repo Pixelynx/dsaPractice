8ea9f3bb0b650b13796583024365f63f41d6227c
pixelynx <tjstevens1992@gmail.com>
2020-04-29 21:25:07 -0400

    add solution

--- javascript/linkedList/firstUniqueInQ.js
+++ javascript/linkedList/firstUniqueInQ.js
@@ -5,6 +5,32 @@
 // - int showFirstUnique() returns the value of the first unique integer of the queue, and returns -1 if there is no such integer.
 // - void add(int value) insert value to the queue.
 
+class FirstUnique {
+    constructor(nums) {
+        this.set = new Set();
+        this.map = new Map();
+        this.buildMap(nums);
+    }
+    buildMap(nums) {
+        nums.forEach(num => this.add(num))
+    }
+    
+    showFirstUnique() {
+        return this.set.size ? this.set.values().next().value : -1;
+    }
+    
+    add(num) {
+       if(this.map.has(num)) {
+            this.map.set(num, this.map.get(num) + 1)
+           this.set.delete(num)
+        } else {
+            this.map.set(num, 1)
+            this.set.add(num)
+        }
+    }
+    
+}
+
 let FirstUnique = function(nums) {
     
 };
