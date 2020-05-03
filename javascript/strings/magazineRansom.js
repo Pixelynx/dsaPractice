0a20bcf5db37a4f650ef31585201df042504cd2a
pixelynx <tjstevens1992@gmail.com>
2020-05-03 19:09:17 -0400

    swipe faster solution

--- javascript/strings/magazineRansom.js
+++ javascript/strings/magazineRansom.js
@@ -1,28 +1,27 @@
-const magazineDic = (magazine) => {
-    let dic = {};
-    for(let letter of magazine) {
-        !dic[letter] ? dic[letter] = 1 : dic[letter]++;
-    }
-    return dic;
-};
+// const magazineDic = (magazine) => {
+//     let dic = {};
+//     for(let letter of magazine) {
+//         !dic[letter] ? dic[letter] = 1 : dic[letter]++;
+//     }
+//     return dic;
+// };
 
-const canConstruct = function(ransomNote, magazine) {
-    let dictionary = magazineDic(magazine);
+// const canConstruct = function(ransomNote, magazine) {
+//     let dictionary = magazineDic(magazine);
     
-    while(ransomNote.length) {
-        let ransomLetter = ransomNote[0];
-        if(dictionary[ransomLetter]) {
-            dictionary[ransomLetter] = dictionary[ransomLetter]-1; ransomNote = ransomNote.slice(1);
-        } else return false;
-    }
-    return true;
-};
+//     while(ransomNote.length) {
+//         let ransomLetter = ransomNote[0];
+//         if(dictionary[ransomLetter]) {
+//             dictionary[ransomLetter] = dictionary[ransomLetter]-1; ransomNote = ransomNote.slice(1);
+//         } else return false;
+//     }
+//     return true;
+// };
 
-let note = "bgz";
-let mag = "efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj";
+// let note = "bgz";
+// let mag = "efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj";
 // let note = "aa";
 // let mag = "aab";
-console.log(canConstruct(note, mag));
 
 // FIRST ATTEMPT
 // let canConstruct = function(ransomNote, magazine) {
@@ -34,3 +33,21 @@ console.log(canConstruct(note, mag));
 //         return canConstruct(ransomNote, magazine.slice(1));
 //     } else return canConstruct(ransomNote.slice(1), magazine.slice(1))
 // };
+
+const canConstruct = function(ransomNote, magazine) {
+    for(let i = 0; i < ransomNote.length; i++){
+        // if letter in note is found in magazine, returns the index else returns -1
+         let index = magazine.indexOf(ransomNote[i]);
+         if(index > -1){
+             magazine = magazine.slice(0,index) + magazine.slice(index + 1);
+         } else return false;
+     }
+     return true;
+ };
+
+let note = "bg";
+let mag = "efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj";
+// let note = "aa";
+// let mag = "aab";
+
+console.log(canConstruct(note, mag));
